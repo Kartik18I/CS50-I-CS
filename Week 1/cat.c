7e934f5dad0a0d03715c02d2f48b509c96d24c1e
Kartik Gaikwad <152002216+Kartik18I@users.noreply.github.com>
2026-07-10 07:02:24 +0000

    Fri, Jul 10, 2026, 12:32 PM +05:30

--- Week 1/cat.c
+++ Week 1/cat.c
@@ -2,18 +2,18 @@
 #include<stdio.h>
 
 //Prototype
-void meow(void);
+void meow(int n);
 
 int main (void)
 {
-    for (int i = 0; i < 3; i++)
-    {
-        meow();
-    }
+    meow(3);
 }
 
 
 void meow(int n)
 {
-    for (int i =)
+    for (int i = 0; i < n; i++)
+{
+    printf("meow\n");
+}
 }
