ef61ab251e17d12a4a2da1e3d9c525e355b8e608
Kartik Gaikwad <152002216+Kartik18I@users.noreply.github.com>
2026-07-10 07:43:37 +0000

    Fri, Jul 10, 2026, 1:13 PM +05:30

--- Week 1/mario.c
+++ Week 1/mario.c
@@ -2,11 +2,12 @@
 
 int main(void)
 {
+    const int n = 3;
     // For each row
-   for (int row = 0; row < 3;row++)
+   for (int row = 0; row < n;row++)
    {
     // for each column
-    for (int column = 0; column < 3; column++)
+    for (int column = 0; column < n; column++)
     {
         //print one brick
 
