b1faad478765f995dcacdaa53012aa42c2c4f079
Kartik Gaikwad <152002216+Kartik18I@users.noreply.github.com>
2026-07-10 06:55:43 +0000

    Fri, Jul 10, 2026, 12:25 PM +05:30

--- Week 1/cat.c
+++ Week 1/cat.c
@@ -1,20 +1,130 @@
 #include<cs50.h>
 #include<stdio.h>
 
-int main(void)
+
+int main (void)
 {
-    int n;
-    while (true)
-{
-    n = get_int("What's n? ");
-   if (n >= 0)
-   {
-    break;
-   }
+    for (int i = 0; i < 3; i++)
+    {
+        meow();
+    }
 }
 
- for (int i = 0; i < n; i++)
- {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void meow(void)
+{
     printf("meow\n");
- }
 }
